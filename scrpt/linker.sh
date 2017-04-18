85653eb043c9ef830624bfc8fc0c19eca1d260ba
Kevin Goldstein <kevin@sendence.com>
2017-04-18 16:28:45 -0700

    commented out unkessary library

--- scrpt/linker.sh
+++ scrpt/linker.sh
@@ -49,55 +49,6 @@ if [ "$WALL_LIB_PATH" == "" ] ; then
 fi
 
 
-#LIBNAME=_PYAPI.so
-#c++ \
-#    -O0 -mcx16 \
-#    -o lib/$LIBNAME \
-#    -fPIC \
-#    -fpermissive \
-#    -g3 \
-#    -ggdb \
-#    -O0 \
-#    -march=native \
-#    -mcx16 \
-#    -shared \
-#    -lpython2.7 \
-#    -lpthread \
-#    -ldl \
-#    -lutil \
-#    -lm \
-#    -std=c++11 \
-#    -DDEBUG \
-#    -I. \
-#    -I/usr/local/include/WallarooCppApi \
-#    -export-dynamic -Wl,-O1 -Wl,-Bsymbolic-functions -std=c++11 -DDEBUG \
-#    $WALL_LIB_PATH/libwallaroo.a \
-#    $PONY_LIB_PATH/lib/libponyrt-pic.a \
-#    ./CMakeFiles/_PYAPI.dir/src/main.cpp.o \
-#    ./CMakeFiles/_PYAPI.dir/src/PyModule.cpp.o \
-#    ./CMakeFiles/_PYAPI.dir/src/PyApplication.cpp.o \
-#    ./CMakeFiles/_PYAPI.dir/libpyapiPYTHON_wrap.cxx.o \
-#    -Wl,--whole-archive $WALL_LIB_PATH/libwallaroo.a -Wl,--no-whole-archive \
-#    -Wl,--whole-archive ./lib/src.o -Wl,--no-whole-archive \
-#    -Wl,-Bstatic -L $WALL_LIB_PATH -lwallaroo -Wl,-Bdynamic \
-#    -Wl,--start-group -lwallaroo -lstdc++ -lrt -Wl,--end-group -lpthread -ldl -lm -lpython2.7 -lstdc++ -lponyrt-pic -lssl
-#if [ $? -gt 0 ] ; then
-#    $print ERR "DID NOT COMPILE $LIBNAME CORRECTLY"
-#    exit 1
-#else
-#    $print INF "LIBRARY finished: $LIBNAME"
-#fi
-
-#
-# create link
-#
-#if [ ! -e ./bin/_PYAPI.so ] ; then
-#    pushd bin >> /dev/null 2>&1
-#    ln -s ../lib/_PYAPI.so
-#    popd >> /dev/null 2>&1
-#    $print INF "Linked $LIBNAME"
-#fi
-
 
 
 PYTHON_FLAGS=`python-config --cflags --ldflags`
