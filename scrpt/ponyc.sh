2f91dff1c00056b4e80f1e5d8fd4bcd2885ea3f3
Kevin Goldstein <kevin@sendence.com>
2017-04-17 19:44:46 -0700

    location dependent

--- scrpt/ponyc.sh
+++ scrpt/ponyc.sh
@@ -1,1 +1,1 @@
-ponyc --debug --pic --pass=obj --path=/home/kgoldstein/dev/projects/wallaroo/lib --output=/home/kgoldstein/dev/c++/palindrome-cpp/lib /home/kgoldstein/dev/projects/palindrome-cpp/src
+ponyc --debug --pic --pass=obj --path=/home/kgoldstein/dev/projects/wallaroo/lib --output=$PWD/lib /home/kgoldstein/dev/projects/palindrome-cpp/src
